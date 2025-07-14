7ed0b73001954b7f026e74659d5953dd936dd141
Abdul Rohim <arohim@students.hacktiv8.ac.id>
2025-07-14 13:25:21 +0700

    fix case sensitivity in import statement for App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app";
 import { BrowserRouter } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./index.css";
